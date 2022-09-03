0d29bfc1d670d82c4093f9f4d1bfd6231d79f839
akash <akashshingavi@gmail.com>
2022-09-03 17:08:55 +0530

    Akash-Second Time

---
+++ src/Practise/Program2.java
@@ -0,0 +1,28 @@
+package Practise;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		
+		Set <Integer> s = new HashSet();
+		s.add(1);
+		s.add(2);
+		s.add(3);
+		s.add(4);
+		s.add(5);
+
+		
+		Iterator <Integer> it=s.iterator();
+		
+		while(it.hasNext())
+		{
+			System.out.println(it.next());
+		}
+	}
+
+}
